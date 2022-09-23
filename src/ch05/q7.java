4e0c1d91f67665ffbcd1905b1f9915b725dcbc1f
admin <admin@PC3>
2022-09-23 11:05:05 +0900

    해결했음! 메인 밖에 static 으로 필드값을 배열로 만들어서 사용함!

--- src/ch05/q7.java
+++ src/ch05/q7.java
@@ -4,24 +4,67 @@ public class q7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		// 주어진 배열 항목에서 최대값을 출력하는 코드를 작성하시오 ? 
-		
-		int[] array = {1,5,3,8,2};
-		// 최대값을 저장하는 변수 a 
+
+		// 주어진 배열 항목에서 최대값을 출력하는 코드를 작성하시오 ?
+
+		int[] array = { 1, 5, 3, 8, 2 };
+		// 최대값을 저장하는 변수 a
 		int a = array[0];
 		for (int i = 0; i < array.length; i++) {
-			if(a < array[i]) {
+			if (a < array[i]) {
 				a = array[i];
 			}
 			System.out.println(a);
-			
+
 		}
 		System.out.println("==================\n");
-		System.out.println("MAX A = "+ a);
-		
-		
-		
+		System.out.println("MAX = " + a);
+		System.out.println("==================\n");
+		// 최소값 구하기!
+		int min = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (min > array[i]) {
+				min = array[i];
+			}
+			System.out.println(min);
+
+		}
+		System.out.println("==================\n");
+		System.out.println("MIN = " + min);
+		System.out.println("==================\n");
+
+		// 오름차순 정렬
+		int temp = 0;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[i] > array[j]) {
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+		for (int b : array) {
+			System.out.print(" , " + b);
+		}
+		System.out.println("\n=========================");
+			// 내림차수 비교
+			int temp2 = 0;
+			for (int i = 0; i < array.length; i++) {
+				for (int j = i + 1; j < array.length; j++) {
+					if (array[i] < array[j]) {
+						temp2 = array[i];
+						array[i] = array[j];
+						array[j] = temp2;
+					}
+				}
+			}
+			for (int e : array) {
+				System.out.print(" , " + e);
+			}
+			
+
+		}
+
 	}
 
-}
--- src/ch05/q9.java
+++ src/ch05/q9.java
@@ -4,54 +4,82 @@ import java.util.Scanner;
 
 public class q9 {
 
+	// 필드 (global) 선언 후 명시적으로 초기화 하지않더라도 jvm이 자동을 ㅗ초기화를 해줌
+	static Scanner sc = new Scanner(System.in);
+	static int[] scores; //
+//	static int[] scores  = null;  자동으로 이렇게 만들어준다는 뜻 ! 
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		// 학생들의 점수를 분석하는 프로그램을 만들려고 합ㄴ디ㅏ. 
-		// 키보드로브터 학생수와 각 학생들의 점수를 입력받고 while 문과 Scanner 와 nextLine() 메소드를 이용해서 
+		// 학생들의 점수를 분석하는 프로그램을 만들려고 합니다.
+		// 키보드로브터 학생수와 각 학생들의 점수를 입력받고 while 문과 Scanner 와 nextLine() 메소드를 이용해서
 		// 최고 점수 및 평균을 출력하는 코드를 작성해 보세요
-		
-		
-		// 스캐너 만들기 
-		Scanner sc = new Scanner(System.in);
-		
-		// 멈추게할 boolean 타입 문자 만들기 
-		boolean trueAndFalse = true ; 
-		
+
+		// 멈추게할 boolean 타입 문자 만들기
+		boolean trueAndFalse = true;
+
 		while (trueAndFalse) {
-			
+
 			System.out.println("---------------------------------------------");
 			System.out.println("1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료");
 			System.out.println("---------------------------------------------");
 			int num = Integer.parseInt(sc.nextLine());
 			System.out.println("선택 > " + num);
-			switch(num) {
-			case 1 : 
+			switch (num) {
+			case 1: // 학생수 입력받기
 				System.out.println("학생수 > ");
 				int num2 = Integer.parseInt(sc.nextLine());
-				int[] scores = new int[num2];
-				break; 
-			case 2 : 
-				for (int i = 0; i < scores.length; i++) {
-					System.out.printf("scores[%d]  > ",i);
-					int score = Integer.parseInt(sc.nextLine());
-					
+				scores = new int[num2];
+				break;
+			case 2: //
+				if (scores == null) {
+					System.out.println("학생수를 먼저 입력해주세요");
+					break;
 				}
-				
-				
-			}
-			
-			
-			
-		}
-		
-		
-		
-		
-		
-		
-		
-	}
+				for (int i = 0; i < scores.length; i++) {
+					System.out.printf("scores[%d]  > ", i);
+					int score = Integer.parseInt(sc.nextLine());
+					// 입력받은 값을 넣어주기
+					scores[i] = score;
+				}
+				break;
 
+			case 3:
+				if (scores == null) {
+					System.out.println("학생수를 먼저 입력해주세요");
+					break;
+				}
+				for (int i = 0; i < scores.length; i++) {
+					System.out.printf("scores[%d] %d\n", i, scores[i]);
+				}
+				break;
+			case 4:
+				if (scores == null) {
+					System.out.println("학생수를 먼저 입력해주세요");
+					break;
+				}
+				// 최고점수 받을 변수 만들기 !
+				int max = 0;
+				for (int i = 0; i < scores.length; i++) {
+					if (max < scores[i]) {
+						// 맥스값 넣어주기
+						max = scores[i];
+					}
+
+				}
+				System.out.println("최고점수 : " + max);
+				System.out.println("평균점수 : " + (double) max / scores.length);
+				break;
+			case 5:
+				System.out.println("프로그램을 종료합니다! ");
+				trueAndFalse = false;
+			default :
+				System.out.println("1~5 의 정수만 입력해주세요 ");
+
+			}
+
+		}
+
+	}
 }
